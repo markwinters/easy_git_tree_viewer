615ec050e11fb6fb3b54f8ae5d052c15c1be041f
Mark Winters <markwinters333@gmail.com>
2016-11-16 20:08:04 +0000

    Depth debug

--- Parser.php
+++ Parser.php
@@ -3,20 +3,15 @@ namespace Markwinters\EasyGitTreeViewer;
 
 class Parser {
 
-    //git log --graph --decorate --oneline
-    // git log --graph --full-history --all \--pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
-    //log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'
     /**
      * @param string $branch
-     * @see https://git-scm.com/docs/pretty-formats
-     *      
+     * @see https://git-scm.com/docs/pretty-formats     *
      */
     public function parse($branch = 'master') {
-        //exec('git log --graph --pretty=format:"H:%H%naN:%aN%naE:%aE%nai:%ai%ncN:%cN%ncE:%cE%nci:%ci%ns:%s%nb:%b%n%n" ' . $branch, $output);
         exec('git log --graph --abbrev-commit --decorate --all --pretty=format:"%n%h%n%aD%n%s%n%d" ' . $branch, $output);
         foreach($output as $linepart) {
-            
-            print_r($linepart);echo '<br />';
+            $depth = substr_count($linepart, '|');
+            print_r($linepart);echo '-';print_r($depth);echo '<br />';
         }
     }
 }
